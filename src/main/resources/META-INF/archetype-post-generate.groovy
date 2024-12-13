6fab85414a012ed49414b87132fbf28d7e897d90
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-12-13 12:41:46 +0100

    switch back to groovy-xml 4.0.23 to be in line what the archetype plugin is using

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Grab(group="org.apache.groovy", module="groovy-xml", version="4.0.24")
+@Grab(group="org.apache.groovy", module="groovy-xml", version="4.0.23")
 
 import java.util.regex.Pattern
 import org.apache.commons.io.FileUtils
